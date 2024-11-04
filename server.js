05fdac62c4b31747a0e60a18557c4ac3863cbc25
ale <delacorte19@gmail.com>
2024-11-04 10:51:19 +0100

    MOD:html con fetch y querys

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 8000
 const { exec } = require('child_process')
 const { error } = require("console")
 const { stdout, stderr } = require("process")
-const comando = "cat >> Hola.txt"
+//const comando = "cat >> Hola.txt"
+const comando = "figlet Hola mundo"
+//const ping = "ping -c4 google.com"
+app.use(express.static("public"))
 
 app.get("/", (req, res) =>{
     exec(comando, (error, stdout, stderr) => {
         console.log(error, stdout, stderr)
         console.log(`stdout:${stdout}`);
-        res.send(stdout)
+        res.send(`<pre>${stdout}</pre>`)
+    })
+})
+
+app.get("/ping", (req, res) =>{
+    const url = req.query.url
+    const comando = `ping -c4 ${url}`
+    exec(comando, (error, stdout, stderr) => {
+        console.log(error, stdout, stderr)
+        console.log(`stdout:${stdout}`);
+        res.send(`<pre>${stdout}</pre>`)
     })
 })
 
