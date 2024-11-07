58246b8441cd675ff6987528e05e0e01047a6df6
ale <delacorte19@gmail.com>
2024-11-07 10:15:03 +0100

    se empieza ha usar figlet

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 8000
 const { exec } = require('child_process')
 const { error } = require("console")
 const { stdout, stderr } = require("process")
+const figlet = require("figlet")
 //const comando = "cat >> Hola.txt"
 const comando = "figlet Hola mundo"
 //const ping = "ping -c4 google.com"
@@ -17,14 +18,49 @@ app.get("/", (req, res) =>{
     })
 })
 
-app.get("/ping", (req, res) =>{
-    const url = req.query.url
-    const comando = `ping -c4 ${url}`
-    exec(comando, (error, stdout, stderr) => {
-        console.log(error, stdout, stderr)
-        console.log(`stdout:${stdout}`);
-        res.send(`<pre>${stdout}</pre>`)
-    })
+ app.get("/ping", (req, res) =>{
+     const url = req.query.url
+     const comando = `ping -c4 ${url}`
+     exec(comando, (error, stdout, stderr) => {
+         console.log(error, stdout, stderr)
+         console.log(`stdout:${stdout}`);
+         res.send(`<pre>${stdout}</pre>`)
+     })
+ })
+app.get("/fuentes", (req, res) =>{
+    figlet.fonts(function (err, fonts) {
+        if (err) {
+          console.log("something went wrong...");
+          console.dir(err);
+          return;
+        }
+        res.send(fonts);
+      });
+})
+
+app.get("/figlet", (req, res) =>{
+    const texto = req.query.texto
+    const fuente = req.query.fuente
+    
+    figlet.text(
+        `${texto}`,
+        {
+          font: "Doom",
+          horizontalLayout: "default",
+          verticalLayout: "default",
+          width: 80,
+          whitespaceBreak: true,
+        },
+        function (err, data) {
+          if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+          }
+          res.send(`<pre>${data}</pre>`);
+        }
+      );
+
 })
 
 app.listen(port, ()=>{
